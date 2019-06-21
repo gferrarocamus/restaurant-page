c3a6516e8889f6e973431da1afed921fd71030ae
Giuliana Ferraro <giulianaferrarocamus@gmail.com>
2019-06-21 07:13:23 +0200

    fix linting errors

--- src/index.js
+++ src/index.js
@@ -4,16 +4,6 @@ import menu from './tabMenu';
 import contact from './tabContact';
 import pageLoad from './pageLoad';
 
-const setActive = (e) => {
-  const tabs = document.getElementsByClassName('tab');
-  [...tabs].forEach((tab) => {
-    tab.classList.remove('active');
-  });
-  const tab = e.target;
-  tab.classList.add('active');
-  pageLoad(getActiveTab(e.target.id));
-};
-
 const getActiveTab = (id) => {
   if (id === 'contact') {
     return contact();
@@ -22,6 +12,16 @@ const getActiveTab = (id) => {
   } else {
     return homepage();
   }
+};
+
+const setActive = (e) => {
+  const tabs = document.getElementsByClassName('tab');
+  [...tabs].forEach((tab) => {
+    tab.classList.remove('active');
+  });
+  const tab = e.target;
+  tab.classList.add('active');
+  pageLoad(getActiveTab(e.target.id));
 };
 
 const initNavbar = () => {
